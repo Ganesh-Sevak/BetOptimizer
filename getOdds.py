97f62ec03ee2b31699877f1a74359659e9ad3a47
Ganesh-Sevak <gsevak585@gmail.com>
2025-07-25 23:11:44 -0500

    added

--- getOdds.py
+++ getOdds.py
@@ -1,8 +1,10 @@
 import requests
 # import warnings
 # warnings.filterwarnings("ignore", category=UserWarning)
+from dotenv import load_dotenv
+import os
 
-API_KEY = '0137f1c0e853984f2682be05eef2b399'
+API_KEY = os.getenv("x")
 sport = 'upcoming'
 region = 'us'  # Options: us, uk, eu, au
 market = 'h2h'  # 'h2h' for moneyline bets
